d381b5907c79e27a547ea682ac81b92d04cbdf3b
Erik Metzig <e.metzig@gmail.com>
2018-07-15 11:45:15 -0400

    Update IsSubSequence.java

--- IsSubSequence.java
+++ IsSubSequence.java
@@ -2,6 +2,7 @@ class IsSubsequence {
     
     public boolean isSubsequence(String s, String t) {
      
+        //handle bad input
         if((s == null || s.length() == 0) && (t == null || s.length() == 0))
             return true;
         
@@ -11,12 +12,15 @@ class IsSubsequence {
         int posSeen = 0;
         boolean foundThisChar = false;
         
+        //for each char in the first string, must find somewhere in second
         for(char c: s.toCharArray())    {
             
             foundThisChar = false;
             
+            //while there's more space to check
             while(posSeen < t.length()) {
                 
+                //if char is found
                 if(t.charAt(posSeen) == c)  {
                     
                     posSeen++;
@@ -28,6 +32,7 @@ class IsSubsequence {
                     posSeen++;
             }
             
+            //false if can't find the char in order
             if(!foundThisChar)
                 return false;
         }
